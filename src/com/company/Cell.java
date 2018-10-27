098a2179dde2a148e5ec90eae22a71448eb5dcc5
PlantedChicken <aric.anderson.aa@gmail.com>
2018-10-26 22:29:44 -0700

    Class Changes
    Cell:
       Added Getters/Setters
       Added varibles
      Added Constructor
    
    Tank:
       Added Getters/Setters
       Added varibles
       Added Constructor
       Added Update Damage
      Added  increase and decrease health

--- src/com/company/Cell.java
+++ src/com/company/Cell.java
@@ -1,4 +1,34 @@
 package com.company;
 
 public class Cell {
+
+    private boolean isHit;
+    private boolean hasTank;
+    private Tank tank;
+
+    public Cell( ) {
+        this.isHit = false;
+        this.hasTank = false;
+        this.tank = null;
+    }
+
+    public boolean isHit() {
+        return isHit;
+    }
+
+    public boolean HasTank() {
+        return hasTank;
+    }
+
+    public void setHasTank(boolean hasTank) {
+        this.hasTank = hasTank;
+    }
+
+    public Tank getTank() {
+        return tank;
+    }
+
+    public void setTank(Tank tank) {
+        this.tank = tank;
+    }
 }
--- src/com/company/Tank.java
+++ src/com/company/Tank.java
@@ -1,4 +1,53 @@
 package com.company;
 
 public class Tank {
+    private int health;
+    private int damage;
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+
+    //Class Methods
+
+    public void updateDamage(){
+        switch (this.health){
+            case 4:
+                damage = 20;
+                break;
+            case 3:
+                damage = 5;
+                break;
+            case 2:
+                damage = 2;
+                break;
+            case 1:
+                damage = 1;
+                break;
+            case 0:
+                damage = 0;
+                break;
+        }
+    }
+
+    public void decreaseHealth(int damage){
+        this.health -= damage;
+    }
+
+    public void increaseHealth(int damage){
+        this.health += damage;
+    }
 }
