8d125e2b47ecf6c5c4c314e19112ceba155bc514
PlantedChicken <aric.anderson.aa@gmail.com>
2018-11-09 17:57:43 -0800

    InputHander changes
    Class will now get and input and return it

--- src/com/fortressdefence/ui/InputHandler.java
+++ src/com/fortressdefence/ui/InputHandler.java
@@ -11,16 +11,34 @@ public class InputHandler {
     }
 
 
-    public void getInput(){
+    public int[] getInput(){
 
         System.out.print("Enter the board location you want to attack: ");
-        String in = this.input.nextLine();
-        char [] inCharArray = in.toCharArray();
-        if((inCharArray[0] >= 'a' && inCharArray[0] <= 'j' || inCharArray[0] >= 'A' && inCharArray[0] <= 'J') && ((inCharArray[1] - '0') >= 1 && (inCharArray[1] - '0') <= 10 ) && !(inCharArray.length > 2)){
-           System.out.println("Test Works");
-        } else{
-            System.out.println("Test Didn't Work");
+        char Row;
+        int Col;
+        // get input and put it in string
+        while(true) {
+            String in = this.input.nextLine();
+
+            //turn string first char and parse int from last chars
+            try {
+                 Row = in.charAt(0);
+                 Col = Integer.parseInt(in.substring(1));
+            } catch (Exception e) { //if it fails to parse an int it throw an exception and sets Row and Col out of bounds
+                Row = 'z';
+                Col = 99;
+            }
+
+            // test if input is within bounds of the board
+            if ((Row >= 'a' && Row <= 'j' || Row >= 'A' && Row <= 'J') && (Col >= 1 && Col <= 10)) {
+                //, and return
+               int[] a = { Row - 'a' + 1, Col};//convert the Row into an int relative to its position in the alphabet
+               return a;
+            } else {
+                System.out.println("Selection invalid. Input must be <letter><number> within the bounds of the board\nEXAMPLE: b5");
+            }
         }
+
     }
 
 }
